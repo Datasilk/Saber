9592aab7dced7fa80ae13e52c5dee7b859cfa2ae
Mark Entingh <entingh@gmail.com>
2018-05-20 00:24:45 -0700

    updated demo website to include logout & favicon

--- App/Content/temp/scripts/website.js
+++ App/Content/temp/scripts/website.js
@@ -1,3 +1,20 @@
-(function() {
-	
+(function () {
+    //log out function
+    window.logout = function () {
+        ajax('api/User/LogOut', function () {
+            window.location.href = '/login';
+        });
+    }
+
+    //simple ajax method
+    function ajax(url, callback) {
+        var req = new XMLHttpRequest();
+        req.onload = function () {
+            if (req.status >= 200 && req.status < 400 && typeof callback == 'function') {
+                callback(req.responseText);
+            }
+        };
+        req.open('GET', url);
+        req.send();
+    }
 })();
