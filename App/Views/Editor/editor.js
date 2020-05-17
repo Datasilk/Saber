ec4aa9ededbd38c49007405ca3a12460c451a987
Mark Entingh <entingh@gmail.com>
2020-05-17 00:07:02 -0700

    can select Scripts to load from Page Settings config

--- App/Views/Editor/editor.js
+++ App/Views/Editor/editor.js
@@ -470,7 +470,7 @@ S.editor = {
 
             submit: function (e) {
                 e.preventDefault();
-                e.cancelBubble;
+                e.cancelBubble = true;
                 var data = {
                     path: $('#newfilepath').val(),
                     filename: $('#newfilename').val().replace(/\s/g, '')
@@ -510,7 +510,7 @@ S.editor = {
 
             submit: function(e) {
                 e.preventDefault();
-                e.cancelBubble;
+                e.cancelBubble = true;
                 var data = {
                     path: $('#newfolderpath').val().replace(/\s/g,''),
                     folder: $('#newfolder').val()
@@ -1310,6 +1310,8 @@ S.editor = {
                     //set up button events
                     $('.page-settings .title-prefix .icon a').on('click', S.editor.settings.title.prefix.show);
                     $('.page-settings .title-suffix .icon a').on('click', S.editor.settings.title.suffix.show);
+                    $('.page-scripts .btn-add-script').on('click', S.editor.settings.scripts.add.show);
+                    $('.scripts-list .close-btn').on('click', S.editor.settings.scripts.remove);
                 }
             );
         },
@@ -1341,7 +1343,7 @@ S.editor = {
 
                 submit: function (e) {
                     e.preventDefault();
-                    e.cancelBubble;
+                    e.cancelBubble = true;
                     var data = { title: $('#page_title_new_prefix').val(), prefix:true };
                     S.ajax.post('PageSettings/CreatePageTitlePart', data,
                         function (d) {
@@ -1364,7 +1366,7 @@ S.editor = {
 
                 submit: function (e) {
                     e.preventDefault();
-                    e.cancelBubble;
+                    e.cancelBubble = true;
                     var data = { title: $('#page_title_new_suffix').val(), prefix: false };
                     S.ajax.post('PageSettings/CreatePageTitlePart', data,
                         function (d) {
@@ -1445,6 +1447,57 @@ S.editor = {
                 }
             }
 
+        },
+
+        scripts: {
+            add: {
+                show: function () {
+                    S.popup.show('Add Script to Page',
+                        $('#template_scripts_add').html()
+                    );
+                    $('.popup form').on('submit', S.editor.settings.scripts.add.submit);
+
+                    //get list of available scripts
+                    S.ajax.post('PageSettings/GetAvailableScripts', {}, (list) => {
+                        var html = [];
+                        list = JSON.parse(list);
+                        for (var x = 0; x < list.length; x++) {
+                            html.push('<option value="' + list[x] + '">' + list[x] + '</option>');
+                        }
+                        $('#available_scripts').html(html.join('\n'));
+
+                    });
+                },
+
+                submit: function(e) {
+                    e.preventDefault();
+                    e.cancelBubble = true;
+                    var data = { file: $('#available_scripts').val(), path: S.editor.path};
+                    S.ajax.post('PageSettings/AddScriptToPage', data, (list) => {
+                        //add script to page
+                        var script = document.createElement('script');
+                        script.src = data.file;
+                        document.body.appendChild(script);
+                        S.editor.files.js.changed = true;
+                        $('.scripts-list > ul').html(list);
+                    });
+                }
+            },
+
+            remove: function (e) {
+                var target = $(e.target);
+                if (!target.hasClass('close-btn')) {
+                    target = target.parents('.close-btn').first();
+                }
+                console.log(target);
+                console.log('remove');
+                var data = { file: target.attr('data-path'), path: S.editor.path };
+                S.ajax.post('PageSettings/RemoveScriptFromPage', data, (list) => {
+                    //add script to page
+                    S.editor.files.js.changed = true;
+                    $('.scripts-list > ul').html(list);
+                });
+            }
         }
     },
 
